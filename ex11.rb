d9fd4b4ea98d1ca8018289c15648d3832e508259
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:45:29 +0530

    ex11.rb

---
+++ ex11.rb
@@ -0,0 +1,9 @@
+print "how old are you?"
+age = gets.chomp
+print "how tall are you?"
+height = gets.chomp
+print "what is your weight?"
+weight = gets.chomp
+
+puts "So you are #{age} years old , #{weight} heavy and #{height} tall. "
+a
