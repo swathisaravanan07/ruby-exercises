2f66830f791e2aefeb2a1e82130b6db1c77a2966
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:42:09 +0530

    ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name= 'Zed A. Shaw'
+my_age = 35
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He is #{my_age} years old."
+puts "He is #{my_height} inches tall."
+puts "He is #{my_weight} pounds heavy."
+puts "Actually thats not too heavy."
+puts "He has got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+#this line is tricky,try to get it exactly right
+puts "if i add #{my_age},#{my_height},#{my_weight} I will get #{my_age + my_height + my_weight}"
