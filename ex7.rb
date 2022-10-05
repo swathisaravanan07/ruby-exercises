eb0c1e08caf6092d83c5bfde3a61c5bc9b887b27
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:43:20 +0530

    ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Marry had a little lamb."
+puts "its fleece was white and #{"snow"}"
+puts "and everywhere that marry went."
+puts "."*10
+#it will print the fullstop 10 times
+
+end1="c"
+end2="h"
+end3="e"
+end4="e"
+end5="s"
+end6="e"
+end7="b"
+end8="u"
+end9="r"
+end10="g"
+end11="e"
+end12="r"
+
+print end1+end2+end3+end4+end5+end6
+puts end7+end8+end9+end10+end11+end12
