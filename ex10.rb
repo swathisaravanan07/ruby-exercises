159abe27a6fd1df22a51b98424a45a0c699c7fa4
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:45:06 +0530

    ex10.rb

---
+++ ex10.rb
@@ -0,0 +1,9 @@
+tabby_cat ="\tI'm tabbed in."
+persian_cat = "I am split \non a line."
+backslash = "i'm \\a\\cat."
+fat_cat = """ i'll do a list: \n\t*catfood \n\t*fishies \n\t*catnip \n\t*grass """
+
+puts tabby_cat
+puts persian_cat
+puts backslash
+puts fat_cat
