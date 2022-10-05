5c583c4695e917739a6d86bc0949cea6fc46333d
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:44:44 +0530

    ex9.rb

---
+++ ex9.rb
@@ -0,0 +1,9 @@
+days = "mon tues wed thurs fri sat sun"
+months = "jan\nfeb\nmar\naprl\nmay\njune\njuly\naug\nsep"
+
+puts "here are the days in a week #{days}"
+puts "here are the months#{months}"
+puts %q{There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.}
