98170d8ca7c4a72b1559bf319e2bb04aec85d442
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:54:58 +0530

    ex33.rb

---
+++ ex33.rb
@@ -0,0 +1,12 @@
+i=0
+numbers=[]
+ while i<6
+   puts"At the top i is #{i}"
+   numbers.push(i)
+
+   i+=1
+   puts "numbers now :",numbers
+   puts "At the bottom i is #{i}"
+ end
+ puts "the numbers:",numbers
+ 
