0392480209b2e0cabcc15769d02b6f07e941b229
swathisaravanan07 <114290147+swathisaravanan07@users.noreply.github.com>
2022-10-05 17:38:43 +0530

    ex2.rb

---
+++ ex2.rb
@@ -0,0 +1,4 @@
+puts "I could have code like this"#comment after this is ignored
+#this sentence will not be taken into the account
+puts "this will run"
+puts "this is a # number"#now the character is considered as a part of sentence
